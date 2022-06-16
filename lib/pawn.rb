5811cc195bdb673e483a54e14c861a8db49e6fb2
nullbr <bmarianoleite3@gmail.com>
2022-06-16 15:05:25 -0300

    corrected pawn possible movements

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -2,21 +2,39 @@
 
 # pawn class to treat each pawn as a node
 class Pawn
+  attr_accessor :position
+
   def initialize(type, position, board_class)
     @type = type
     @position = position
     @board = board_class
+    @first_postion = position
   end
 
   def pawn_moves
-    directions = [[1, 0], [2, 0], [-1, 0], [-2, 0]]
+    directions = possible_directions
     moves = []
     directions.each do |direction|
       y = @position[0] + direction[0]
       x = @position[1] + direction[1]
+      break unless @board.grid[y][x].nil?
+
       moves << [y, x] if y.between?(0, 7) && x.between?(0, 7)
     end
     moves
   end
-end
 
+  private
+
+  def possible_directions
+    if @type == :black && @first_postion == @position
+      [[-1, 0], [-2, 0]]
+    elsif @type == :black
+      [[-1, 0]]
+    elsif @type == :white && @first_postion == @position
+      [[1, 0], [2, 0]]
+    else
+      [[1, 0]]
+    end
+  end
+end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -3,17 +3,39 @@ require './lib/board'
 
 RSpec.describe Pawn do
   describe '#pawn_moves' do
-    context 'board is empty:' do
+    context 'returns the possible moves of a white pawn:' do
       board = Board.new
-      it 'returns the possible moves of a white pawn' do
-        pawn = Pawn.new(:white, [5, 5], board)
-        expect(pawn.pawn_moves).to eq([[6, 5], [7, 5], [4, 5], [3, 5]])
+      pawn = Pawn.new(:white, [2, 2], board)
+
+      it 'first move' do
+        expect(pawn.pawn_moves).to eq([[3, 2], [4, 2]])
       end
 
-      it 'returns the possible moves of a black pawn' do
-        pawn = Pawn.new(:black, [0, 0], board)
-        expect(pawn.pawn_moves).to eq([[1, 0], [2, 0]])
+      it 'second move' do
+        pawn.position = [4, 2]
+        expect(pawn.pawn_moves).to eq([[5, 2]])
       end
+    end
+
+    context 'returns the possible moves of a black pawn:' do
+      board = Board.new
+      pawn = Pawn.new(:black, [6, 2], board)
+
+      it 'first move' do
+        expect(pawn.pawn_moves).to eq([[5, 2], [4, 2]])
+      end
+
+      it 'second move' do
+        pawn.position = [4, 2]
+        expect(pawn.pawn_moves).to eq([[3, 2]])
+      end
+    end
+
+    it "cannot move somewhere there's a piece in" do
+      board = Board.new
+      board.grid[3][2] = "there's a piece here"
+      pawn = Pawn.new(:white, [2, 2], board)
+      expect(pawn.pawn_moves).to eq([])
     end
   end
 end
