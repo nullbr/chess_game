3db41b222cfdf9adafdcb822065794f77129ad38
nullbr <bmarianoleite3@gmail.com>
2022-06-16 11:52:23 -0300

    tdd knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -14,7 +14,7 @@ class Knight
     directions.each do |direction|
       y = @position[0] + direction[0]
       x = @position[1] + direction[1]
-      moves << [y, x] if y.between?(0, 7) && x.between?(0, 7) && @board.grid[y][x].nil?
+      moves << [y, x] if y.between?(0, 7) && x.between?(0, 7)
     end
     moves
   end
--- lib/pawn.rb
+++ lib/pawn.rb
@@ -16,7 +16,7 @@ class Pawn
     directions.each do |direction|
       y = @position[0] + direction[0]
       x = @position[1] + direction[1]
-      moves << [y, x] if y.between?(0, 7) && x.between?(0, 7) && @board.grid[y][x].nil?
+      moves << [y, x] if y.between?(0, 7) && x.between?(0, 7)
     end
     moves
   end
