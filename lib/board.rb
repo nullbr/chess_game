a71364d4e4fd735967a822280d0f31ae90676de2
nullbr <bmarianoleite3@gmail.com>
2022-06-17 19:48:58 -0300

    make chess a sub class of board

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 require 'colorize'
+require_relative 'king'
+require_relative 'queen'
+require_relative 'knight'
+require_relative 'bishop'
+require_relative 'rook'
+require_relative 'pawn'
 
 # Chess board with colorize gem
 class Board
   attr_accessor :grid
 
-  def initialize
+  def initialize(player1, player2 = nil)
+    @player1 = player1
+    @pleyer2 = player2
     @grid = []
     8.times { @grid << [nil, nil, nil, nil, nil, nil, nil, nil] }
+    set_initial_positions
   end
 
   def to_s
@@ -26,6 +35,48 @@ class Board
       print "\n"
     end
     puts '   a  b  c  d  e  f  g  h'
+  end
+
+  private
+
+  def set_initial_positions
+    pawns && rooks && queens && kings && knights && bishops
+  end
+
+  def pawns
+    8.times { |n| @grid[1][n] = Pawn.new(:white, [2, n], self) }
+    8.times { |n| @grid[6][n] = Pawn.new(:black, [6, n], self) }
+  end
+
+  def rooks
+    @grid[0][0] = Rook.new(:white, [0, 0], self)
+    @grid[0][7] = Rook.new(:white, [0, 7], self)
+    @grid[7][0] = Rook.new(:black, [7, 0], self)
+    @grid[7][7] = Rook.new(:black, [7, 7], self)
+  end
+
+  def queens
+    @grid[0][3] = Queen.new(:white, [0, 3], self)
+    @grid[7][3] = Queen.new(:black, [7, 3], self)
+  end
+
+  def kings
+    @grid[0][4] = King.new(:white, [0, 4], self)
+    @grid[7][4] = King.new(:black, [7, 4], self)
+  end
+
+  def knights
+    @grid[0][1] = Knight.new(:white, [0, 1], self)
+    @grid[0][6] = Knight.new(:white, [0, 6], self)
+    @grid[7][1] = Knight.new(:black, [7, 1], self)
+    @grid[7][6] = Knight.new(:black, [7, 6], self)
+  end
+
+  def bishops
+    @grid[0][2] = Bishop.new(:white, [0, 2], self)
+    @grid[0][5] = Bishop.new(:white, [0, 5], self)
+    @grid[7][2] = Bishop.new(:black, [7, 2], self)
+    @grid[7][5] = Bishop.new(:black, [7, 5], self)
   end
 end
 
--- lib/chess.rb
+++ lib/chess.rb
@@ -1,65 +1,14 @@
 # frozen_string_literal: true
 
 require_relative 'board'
-require_relative 'king'
-require_relative 'queen'
-require_relative 'knight'
-require_relative 'bishop'
-require_relative 'rook'
-require_relative 'pawn'
 
 # main driver class frot the chess game
-class Chess
+class Chess < Board
   def initialize(player1, player2 = nil)
-    @player1 = player1
-    @pleyer2 = player2
-  end
-
-  def set_initial_positions
-    @board = Board.new
-    pawns && rooks && queens && kings && knights && bishops
-    @board.to_s
-  end
-
-  private
-
-  def pawns
-    8.times { |n| @board.grid[1][n] = Pawn.new(:white, [2, n], @board) }
-    8.times { |n| @board.grid[6][n] = Pawn.new(:black, [6, n], @board) }
-  end
-
-  def rooks
-    @board.grid[0][0] = Rook.new(:white, [0, 0], @board)
-    @board.grid[0][7] = Rook.new(:white, [0, 7], @board)
-    @board.grid[7][0] = Rook.new(:black, [7, 0], @board)
-    @board.grid[7][7] = Rook.new(:black, [7, 7], @board)
-  end
-
-  def queens
-    @board.grid[0][3] = Queen.new(:white, [0, 3], @board)
-    @board.grid[7][3] = Queen.new(:black, [7, 3], @board)
-  end
-
-  def kings
-    @board.grid[0][4] = King.new(:white, [0, 4], @board)
-    @board.grid[7][4] = King.new(:black, [7, 4], @board)
-  end
-
-  def knights
-    @board.grid[0][1] = Knight.new(:white, [0, 1], @board)
-    @board.grid[0][6] = Knight.new(:white, [0, 6], @board)
-    @board.grid[7][1] = Knight.new(:black, [7, 1], @board)
-    @board.grid[7][6] = Knight.new(:black, [7, 6], @board)
-  end
-
-  def bishops
-    @board.grid[0][2] = Bishop.new(:white, [0, 2], @board)
-    @board.grid[0][5] = Bishop.new(:white, [0, 5], @board)
-    @board.grid[7][2] = Bishop.new(:black, [7, 2], @board)
-    @board.grid[7][5] = Bishop.new(:black, [7, 5], @board)
+    super(player1, player2)
   end
 end
 
 game = Chess.new('bruno')
 
-game.set_initial_positions
+game.to_s
