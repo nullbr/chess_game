03e4dcabc62ce604015ec8f7a4689188f29e5fe6
nullbr <bmarianoleite3@gmail.com>
2022-06-24 20:41:59 -0300

    saving game and geting input with process data module

---
+++ lib/process_data.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+module ProcessData
+  def save_game(game, filename)
+    Dir.mkdir('saved') unless Dir.exist?('saved')
+    dumpfile = Psych.dump(game)
+    File.write(filename, dumpfile)
+  end
+
+  # Choose from saved games, return filename if exists, return nil if it does not
+  def choose_saved_game
+    options = []
+    saved_games = Dir.entries('saved')[2..-1]
+
+    saved_games.each_with_index do |filename, idx|
+      puts "#{idx}: #{filename}"
+      options << idx
+    end
+    choose_file = yield(options)
+    saved_games[choose_file]
+  end
+
+  def load_game(filename)
+    Psych.load_file(
+      filename,
+      aliases: true,
+      permitted_classes: [Chess, Symbol, Rook, Bishop, Pawn, Queen, King, Knight]
+    )
+  end
+
+  def get_input(lang, options = ' ')
+    yield if block_given?
+    while true
+      input = gets.chomp.strip
+      if %w[quit end exit sair].include?(input.downcase)
+        puts lang.zero? ? 'Your game was saved,' : 'Seu jogo foi salvo,'
+        abort(lang.zero? ? 'Exiting the game...' : 'Saindo do jogo...')
+
+      # Check if theres only letters in the string if options are string
+      elsif options == ' '
+        break
+
+      # Check if string only contains an integer and its the correct type
+      elsif options != ' ' && input !~ /\D/
+        input = input.to_i
+        break if options.include?(input)
+
+        print lang.zero? ? "Options: #{options} " : "Opções: #{options} "
+
+      # Ask for a valid input type
+      else
+        print lang.zero? ? 'Enter a valid. ' : 'Insira opção válida. '
+      end
+    end
+    input
+  end
+end
--- main.rb
+++ main.rb
@@ -1,27 +1,67 @@
 require_relative '.bundle/ruby/2.5.0/gems/ruby_figlet-0.6.1/lib/ruby_figlet'
 require_relative 'lib/chess'
+require_relative 'lib/process_data'
 
 using RubyFiglet # For String.new(...).art / .art! Moneky Patches
+include ProcessData
 
 puts 'Chess'.art
 puts "\t\t   Bruno Leite"
-
 puts "\nLets Play!"
-gets.chomp
 
-player1 = 'bruno'
-player2 = 'giu'
-game = Chess.new(player1, player2)
+lang = get_input(lang, [0, 1]) { puts "What language would you like to play in?\n0: English\n1: Português" }
+
+if Dir.exist?('saved') && Dir.entries('saved').size > 2
+  if lang.zero?
+    puts "\n0: to start a new game \n1: to load a game"
+  else
+    puts "\n0: Começar um novo jogo.\n1: Carregar jogo salvo"
+  end
+  choice = get_input(lang, [0, 1])
+else
+  choice = 0
+end
+
+# Start a new game
+if choice.zero?
+  puts lang.zero? ? "\nName of Player 1" : "\nNome do jogador 1"
+  player1 = get_input(lang)
+
+  puts lang.zero? ? "\nName of Player 2" : "\nNome do jogador 2"
+  player2 = get_input(lang)
+
+  game = Chess.new(player1, player2)
+  system 'clear'
+
+  # Set new file to be used for saving the game
+  time = Time.now.strftime('%Hh%M-%d-%m-%Y')
+  filename = "saved/#{player1}-#{player2}-#{time}.yaml"
+
+# Load a game from saved games
+else
+  if lang.zero?
+    puts "Getting saved games...\nChoose from saved games: "
+  else
+    puts "Carregando jogos....\nEscolha um dos jogos salvos: "
+  end
+  filename = choose_saved_game { |game_options| get_input(lang, game_options) }
+  filename = "saved/#{filename}"
+  game = load_game(filename)
+
+  system 'clear'
+end
 
 while game.checkmate? == false
   puts ''
   game.to_s
   puts "\nCheck!\n" if game.check?
   puts "#{game.current_player[:name]}'s (#{game.current_player[:pieces]}) move:"
-  input = gets.chomp
+  input = get_input(lang)
   game.move_piece(input)
+  save_game(game, filename)
   system 'clear'
 end
 
-game.to_s
-puts 'Checkmate biitches!'
+File.delete(filename) if File.exist?(filename)
+puts 'Checkmate!'
+puts "#{game.next_player} won!"
