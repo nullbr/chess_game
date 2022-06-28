ef2bc93c6fc2ede928d76f3da680d71e5d2cae27
nullbr <bmarianoleite3@gmail.com>
2022-06-28 19:49:48 -0300

    ai moves random piece

--- lib/ai.rb
+++ lib/ai.rb
@@ -2,20 +2,26 @@
 
 # Artificial Inteligence
 class AI
+  # calculates all possible moves and returns a randomily selected one
   def random_move(pieces, grid)
     pieces = pieces[:black]
     possible_moves = []
 
     pieces.each do |piece|
       moves = piece.moves(grid)
-      possible_moves << moves unless moves.empty?
+      possible_moves += moves.map { |move| move + [piece.notation] } unless moves.empty?
     end
-    
-    possible_moves.sample.sample
+    translate(possible_moves.sample)
   end
 
   private
 
-  def make_move(piece, dest, grid)
+  # grabs x
+  def translate(move)
+    y = move[0] + 1
+    x = (move[1] + 97).chr
+    capturing = move[2] ? 'x' : ''
+    notation = move[3] == 'P' ? '' : move[3] 
+    "#{notation}#{capturing}#{x}#{y}"
   end
 end
