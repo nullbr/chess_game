082dc021135c6fae588f3a823485ebbaaff6f8ef
nullbr <bmarianoleite3@gmail.com>
2022-06-30 13:25:15 -0300

    workig on ai

--- lib/ai.rb
+++ lib/ai.rb
@@ -9,38 +9,76 @@ class AI
     @name = 'Machine'
   end
 
-  def choose_move(pieces, grid)
-    random_move(pieces, grid)
-  end
-
-  # calculates all possible moves and returns a randomily selected one
-  def random_move(pieces, grid)
-    pieces = pieces[:black]
-    possible_moves = []
-
-    pieces.each do |piece|
-      moves = piece.moves(grid)
-      possible_moves += moves.map { |move| move + [piece.notation] } unless moves.empty?
+  def move(pieces, grid)
+    @pieces = pieces
+    @grid = grid
+    @moves = possible_moves
+    if @difficulty.zero?
+      move = select_random
+    elsif @difficulty == 1
+      move = select_capturing
     end
-    move = select_capturing(possible_moves)
-    translate(move)
+
+    translate(move) unless move.nil?
   end
 
   private
+
+  # returns array of all moves pieces can make
+  def possible_moves(type = :black)
+    possible_moves = []
+    @pieces[type].each do |piece|
+      moves = piece.moves(@grid)
+      possible_moves += moves.map { |move| move + [piece.notation] + [piece.position[1]] } unless moves.empty?
+    end
+
+    possible_moves
+  end
 
   # grabs x
   def translate(move)
     y = move[0] + 1
     x = (move[1] + 97).chr
     capturing = move[2] ? 'x' : ''
-    notation = move[3] == 'P' ? '' : move[3]
-    "#{notation}#{capturing}#{x}#{y}"
+    if move[3] == 'P'
+      notation = x_origin = ''
+    else
+      notation = move[3]
+      x_origin = (move[4] + 97).chr
+    end
+    "#{notation}#{x_origin}#{capturing}#{x}#{y}"
+  end
+
+  # return a sample move
+  def select_random
+    @moves.sample
   end
 
   # choose move, select one with capturing if available
-  def select_capturing(possible_moves)
-    return possible_moves.sample unless @difficulty > 1 || possible_moves.any? { |move| move[2] == true }
+  def select_capturing
+    can_capture = capturing
+    return select_random if can_capture.empty?
 
-    possible_moves.select { |move| move[2] == true }.sample
+    king_pos = capture_king
+    king_pos.empty? ? can_capture.sample : king_pos
+  end
+  
+  # return kings position if it is available, return empty otherwise
+  def capture_king
+    @moves.each { |move| return move if @grid[move[0]][move[1]].instance_of?(King) }
+  end
+  
+  # return all pieces that can capture
+  def capturing
+    @moves.select { |move| move[2] == true }
+  end
+
+  def select_defense
+    possible_capture = []
+    
+    importance = { 'P': 0, 'B': 1, 'N': 2, 'R': 3, 'Q': 4, 'K': 5 }
+    # @pieces[:black].each { |piece| current_posistions << piece.position + [importance[piece.notation]] }
+    possible_moves(:white).each { || }
+
   end
 end
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -7,13 +7,40 @@ RSpec.describe Bishop do
     it 'makes a random, legal move' do
       ai = AI.new(0)
       chess.next_player
-      expect(chess.move_piece(ai.random_move(chess.all_pieces, chess.grid))).to be_truthy
+      expect(chess.move_piece(ai.move(chess.all_pieces, chess.grid))).to be_truthy
     end
 
     it 'makes a random move selecting capturing' do
       ai = AI.new(1)
       chess.next_player
-      expect(chess.move_piece(ai.random_move(chess.all_pieces, chess.grid))).to be_truthy
+      expect(chess.move_piece(ai.move(chess.all_pieces, chess.grid))).to be_truthy
+    end
+
+    it 'makes a random move selecting king if available' do
+      ai = AI.new(1)
+      game = Chess.new('bruno', ai)
+      game.move_piece('f3')
+      game.move_piece('e5')
+      game.move_piece('a3')
+      game.move_piece('Qh4')
+      game.move_piece('b3')
+      puts ''
+      game.to_s
+      expect(ai.move(game.all_pieces, game.grid)).to eq('Qhxe1')
+    end
+
+    it 'pawn deffends king' do
+      ai = AI.new(2)
+      game = Chess.new('bruno', ai)
+      game.move_piece('e4')
+      game.move_piece('f6')
+      game.move_piece('a6')
+      game.move_piece('e3')
+      game.move_piece('a7')
+      game.move_piece('Qh5')
+      puts ''
+      game.to_s
+      expect(ai.move(game.all_pieces, game.grid)).to eq('g6')
     end
   end
 end
